eb3b54f8834046854c323852273e0c99ae64fc60
Aenariss <aenariss@gmail.com>
2021-03-27 17:59:13 +0100

    edit code to comply with pylint standad

--- src/math_library.py
+++ src/math_library.py
@@ -1,42 +1,92 @@
-## @package math_library
-#  Author: Vojtech Fiala <xfiala61>
-#  Documentation for this module.
-#
+""" @package math_library
+    @author: Vojtech Fiala <xfiala61>
+Documentation for math library.
+"""
+
+class Error(Exception):
+    """ @brief Initialize custom error class.
+        @param Exception To use with a custom exception.
+    """
+    pass
+
+class InvalidSystem(Error):
+    """ @brief Create custom exception.
+        @param From the Error class.
+    """
+    pass
 
 def convert_reverse(number, system):
+    """ @brief Convert number from given system to decimal.
+        @param number The number to convert.
+        @param system Can be any system from 2-16.
+    """
     return True
 
 def convert(number, system, acc=5):
+    """ @brief Convert number from decimal to given system.
+        @param number The number to convert.
+        @param system Can be any system from 2-16.
+        @param acc Accuracy used to count decimal parts. Defaults to 5.
+    """
     return True
 
 def factorial(number):
+    """ @brief Calculate factorial of a number.
+        @param number The number of which factorial to calculate.
+    """
     if number < 0 or isinstance(number, float):
         raise ValueError
 
     result = 1
-    for i in range(0, number):
-        result *= number
-        number -= 1
+    for i in range(1, number+1):
+        result *= i
+
     return result
 
 def add(number, number2):
-    return True
+    """ @brief Add two numbers.
+        @param number Operand 1.
+        @param number2 Operand 2.
+    """
+    return number + number2
 
 def sub(number, number2):
+    """ @brief Substract two numbers.
+        @param number Operand 1.
+        @param number2 Operand 2.
+    """
     return number - number2
 
 def multiply(number, number2):
+    """ @brief Multiply two numbers.
+        @param number Operand 1.
+        @param number2 Operand 2.
+    """
     return number * number2
 
 def divide(number, number2):
+    """ @brief Divide two numbers.
+        @param number Operand 1.
+        @param number2 Operand 2.
+    """
     return number / number2
 
 def exp(number, number2):
-    if number2 < 0:
+    """ @brief Calculate exponent value of a number.
+        @param number Base number.
+        @param number2 The exponent.
+    The base number must be a positive integer or 0.
+    """
+    if number2 < 0 or isinstance(number2, float):
         raise ValueError
     return pow(number, number2)
 
 def sqrt(number, number2):
-    if number < 0:
+    """ @brief Calculate root value of a number.
+        @param number Base number.
+        @param number2 The n-th root.
+    The base number must be a positive integer or 0.
+    """
+    if number < 0 or isinstance(number2, float):
         raise ValueError
     return pow(number, 1/number2)
--- src/math_library_tests.py
+++ src/math_library_tests.py
@@ -1,19 +1,20 @@
-## @package math_library_tests
-#  @author: Vojtech Fiala <xfiala61>
-#  Documentation for math library tests.
-
-from math_library import *
+""" @package math_library_tests
+    @author: Vojtech Fiala <xfiala61>
+Documentation for math library tests.
+"""
 import unittest
+from math_library import add, sub, multiply, divide, InvalidSystem
+from math_library import exp, sqrt, factorial, convert, convert_reverse
 
-## Test class to run the tests.
-#
-#  @param unittest.TestCase Used to get access to test methods.
 class TestStringMethods(unittest.TestCase):
+    """ @brief Test class to run the tests.
+        @param unittest.TestCase Used to get access to test methods.
+    """
 
-    ## @brief Method to test the addition (+)
-    #
-    #  @param self Self object.
     def test_addition(self):
+        """ @brief Method to test the addition (+).
+            @param self Self object.
+        """
         self.assertEqual(add(5, 5), 10)
         self.assertEqual(add(5, 15009), 15014)
         self.assertEqual(add(0, 0), 0)
@@ -22,10 +23,10 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(add(-5, -5), -10)
         self.assertEqual(add(0.5, 5.8), 6.3)
 
-    ## @brief Method to test the substraction (-)
-    #
-    #  @param self Self object.
     def test_substraction(self):
+        """ @brief Method to test the substraction (-).
+            @param self Self object.
+        """
         self.assertEqual(sub(5, 5), 0)
         self.assertEqual(sub(5, 15009), -15004)
         self.assertEqual(sub(0, 0), 0)
@@ -34,10 +35,11 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(sub(-5, -5), 0)
         self.assertEqual(sub(0.5, 5.8), -5.3)
 
-    ## @brief Method to test the multiplication (*)
-    #
-    #  @param self Self object.
+
     def test_multiplication(self):
+        """ @brief Method to test the multiplication (*).
+            @param self Self object.
+        """
         self.assertEqual(multiply(5, 5), 25)
         self.assertEqual(multiply(5, 15009), 75045)
         self.assertEqual(multiply(0, 0), 0)
@@ -47,10 +49,10 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(multiply(-5, 5), -25)
         self.assertEqual(multiply(0.5, 5.8), 2.9)
 
-    ## @brief Method to test the division (/)
-    #
-    #  @param self Self object.
     def test_division(self):
+        """ @brief Method to test the division (/).
+            @param self Self object.
+        """
         self.assertEqual(divide(5, 5), 1)
         self.assertEqual(divide(5, 100), 0.05)
         self.assertRaises(ZeroDivisionError, divide, 0, 0)
@@ -60,27 +62,28 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(divide(-5, 5), -1)
         self.assertEqual(divide(8.5, 0.5), 17)
 
-    ## @brief Method to test the exponent (^)
-    #  The exponent can only be a natural number, which includes 0  
-    #
-    #  @param self Self object.
     def test_exp(self):
+        """ @brief Method to test the exponent (^).
+            @param self Self object.
+
+        The exponent can only be a natural number, which includes 0.
+        """
         self.assertEqual(exp(5, 5), 3125)
         self.assertEqual(exp(5, 2), 25)
         self.assertEqual(exp(0, 5), 0)
         self.assertEqual(exp(5, 0), 1)
         self.assertEqual(exp(-5, 5), -3125)
-        self.assertEqual(exp(9, 0.5), 3)
+        self.assertRaises(ValueError, exp, 9, 0.5)
         self.assertEqual(exp(2, 31), 2147483648)
         self.assertRaises(ValueError, exp, 654, -5)
 
-
-    ## @brief Method to test the root (√)
-    #  The root function doesn't support imaginary numbers, the operand must be a real number
-    #  This function supports n-th root. The 'n' can be any integer.
-    #
-    #  @param self Self object.
     def test_sqrt(self):
+        """ @brief Method to test the root (√).
+            @param self Self object.
+        The root function doesn't support imaginary numbers.
+        The operand must be a real number.
+        This function supports n-th root. The 'n' can be any integer.
+        """
         self.assertEqual(sqrt(9, 2), 3)
         self.assertEqual(sqrt(27, 3), 3)
         self.assertEqual(sqrt(0, 5), 0)
@@ -92,24 +95,23 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(sqrt(9, 1), 9)
 
 
-    ## @brief Method to test the factorial (!)
-    #  Factorial operand can be any integer higher than 0.
-    #
-    #  @param self Self object.
     def test_fact(self):
+        """ @brief Method to test the factorial (!)
+            @param self Self object.
+        Factorial operand can be any integer higher than 0.
+        """
         self.assertEqual(factorial(9), 362880)
         self.assertEqual(factorial(5), 120)
         self.assertEqual(factorial(0), 1)
         self.assertRaises(ValueError, factorial, -1)
         self.assertRaises(ValueError, factorial, 5.5)
 
-
-    ## @brief Method to test the number system convertor.
-    #  The convertor will support conversion from decimal system to any given system (2-16).
-    #  The convertor will also support reverse conversion from any given system (2-16) to decimal.
-    #
-    #  @param self Self object.
     def test_convert(self):
+        """ @brief Method to test the number system convertor.
+            @param self Self object.
+        Supports conversion from decimal system to any given system (2-16).
+        Supports reverse conversion from any given system (2-16) to decimal.
+        """
         self.assertEqual(convert(9, 2), "1001")
         self.assertEqual(convert(0, 2), "0")
         self.assertEqual(convert(16, 2), "10000")
@@ -136,11 +138,12 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(convert_reverse(4556, 10), 4556)
         self.assertEqual(convert_reverse(4556.0, 10), 4556)
         self.assertEqual(convert_reverse(4556., 10), 4556)
-        self.assertEqual(convert_reverse(4556568465468, 10), 4556568465468)
-        self.assertEqual(convert_reverse(6545618.654564561, 10), 6545618.654564561)
+        self.assertEqual(
+            convert_reverse(4556568465468, 10), 4556568465468)
+        self.assertEqual(
+            convert_reverse(6545618.654564561, 10), 6545618.654564561)
         self.assertEqual(convert_reverse('345A.85A', 16), 13402.52197265625)
         self.assertEqual(convert_reverse('345A.85A', 11), 4542.7761081893313298272)
-
 
 if __name__ == '__main__':
     unittest.main()
